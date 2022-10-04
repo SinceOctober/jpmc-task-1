b1888a65a7ba39abdf5c61354d558a39edae33c0
SinceOctober <52226070+SinceOctober@users.noreply.github.com>
2022-10-04 17:15:29 +0100

    Update client.py

--- client.py
+++ client.py
@@ -35,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = 0.5 * (bid_price / ask_price)
     return stock, bid_price, ask_price, price
 
 
@@ -59,4 +59,4 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(price['ABC'], price['DEF']))
